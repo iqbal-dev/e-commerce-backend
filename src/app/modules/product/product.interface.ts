2d075eaa8d9636f83d385191a92fa3c05fd5212e
iqbal-dev <pkmdiqbal@gmail.com>
2024-05-20 22:51:51 +0600

    [add] product interface

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,17 @@
+export type TVariant = {
+  type: string
+  value: string
+}
+
+export type TInventory = {
+  quantity: number
+  inStock: boolean
+}
+export type TProduct = {
+  name: string
+  description: string
+  price: number
+  tags: string[]
+  variants: TVariant[]
+  inventory: TInventory
+}
