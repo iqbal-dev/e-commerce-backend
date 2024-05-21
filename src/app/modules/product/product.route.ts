0e5d35e842d575c7e30ea3d5d2abb3089540e786
iqbal-dev <pkmdiqbal@gmail.com>
2024-05-22 01:49:11 +0600

    update the code

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import { ProductController } from './product.controller';
 
 const router = express.Router();
 
-router.route('/').get((_req, res) => {
-  res.json({
-    success: true,
-    message: 'Hello world',
-  });
-});
+router
+  .route('/')
+  .get((_req, res) => {
+    res.json({
+      success: true,
+      message: 'Hello world',
+    });
+  })
+  .post(ProductController.create);
 
 const productRoute = router;
 
